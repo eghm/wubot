c7e14ebac02f8ade2e6dcdfd47e4c12b23141a54
Alex White <VVu@geekfarm.org>
2011-10-12 00:56:15 -0700

    tweaks to State reactor and test case
      - 'cache data not updated' status set to critical
      - 'warning' text removed from subject, duplicated text added by Status reactor
      - sort state files for predictable output for test cases

--- lib/App/Wubot/Reactor/State.pm
+++ lib/App/Wubot/Reactor/State.pm
@@ -160,12 +160,20 @@ sub monitor {
 
     my $directory = $self->cachedir;
 
+    my @files;
+
     my $dir_h;
     opendir( $dir_h, $directory ) or die "Can't opendir $directory: $!";
-
-  FILE:
     while ( defined( my $entry = readdir( $dir_h ) ) ) {
         next unless $entry && $entry =~ m|\.yaml$|;
+
+        push @files, $entry;
+    }
+    closedir( $dir_h );
+
+
+  FILE:
+    for my $entry ( sort @files ) {
 
         my $cache = YAML::XS::LoadFile( "$directory/$entry" );
 
@@ -202,23 +210,19 @@ sub monitor {
                 YAML::XS::DumpFile( "$directory/$entry", $cache );
 
                 my $check_age_string = $self->timelength->get_human_readable( $check_age );
-                my $warning = "Warning: cache data for $key:$field not updated in $check_age_string";
-                $self->logger->warn( $warning );
+                my $error = "cache data for $key:$field not updated in $check_age_string";
+                $self->logger->warn( $error );
 
                 push @react, { key        => $key,
-                               status     => 'WARNING',
-                               subject    => $warning,
+                               status     => 'CRITICAL',
+                               subject    => $error,
                                lastupdate => $now,
                                mailbox    => $mailbox,
                            };
 
             }
         }
-
-
     }
-    closedir( $dir_h );
-
 
     return unless @react;
 
